d315bed85920c8d9826721def256ad4c812f5487
Thanh Nguyen <tienthanh8490@gmail.com>
2021-05-17 01:53:47 +0200

    test remove then add

--- test/index.test.js
+++ test/index.test.js
@@ -13,13 +13,31 @@ describe('LWWSet', () => {
   })
 
   describe('basic add and remove', () => {
-    test('should be able to add an entirely new element', () => {
+    test('should be able to add a new element', () => {
       lwwSet.add('a')
       
       expect(lwwSet.lookup('a')).toBe(true)
     })
 
-    test('should be able to update an existing element with newer timestamp', () => {
+    test('should be able to remove an existing element', () => {
+      lwwSet.add('a', 1)
+      
+      expect(lwwSet.lookup('a')).toBe(true)
+
+      lwwSet.remove('a', 2)
+
+      expect(lwwSet.lookup('a')).toBe(false)
+    })
+
+    test('should be able to remove a non-existing element', () => {
+      lwwSet.remove('a', 1)
+      
+      expect(lwwSet.lookup('a')).toBe(false)
+      expect(lwwSet.addSet.get('a')).toBe(undefined)
+      expect(lwwSet.removeSet.get('a')).toBe(1)
+    })
+
+    test('should be able to add an element multiple times and keep the latest timestamp', () => {
       lwwSet.add('a', 1)
       
       expect(lwwSet.lookup('a')).toBe(true)
@@ -31,7 +49,15 @@ describe('LWWSet', () => {
       expect(lwwSet.addSet.get('a')).toBe(2)
     })
 
-    test('should be able to remove an existing element', () => {
+    test('should be able to remove an element multiple times and keep the latest timestamp', () => {
+      lwwSet.remove('a', 1)
+      
+      expect(lwwSet.lookup('a')).toBe(false)
+      expect(lwwSet.addSet.get('a')).toBe(undefined)
+      expect(lwwSet.removeSet.get('a')).toBe(1)
+    })
+
+    test('should be able to lookup an element that was removed then added again later', () => {
       lwwSet.add('a', 1)
       
       expect(lwwSet.lookup('a')).toBe(true)
@@ -39,6 +65,10 @@ describe('LWWSet', () => {
       lwwSet.remove('a', 2)
 
       expect(lwwSet.lookup('a')).toBe(false)
+
+      lwwSet.add('a', 3)
+
+      expect(lwwSet.lookup('a')).toBe(true)
     })
   })
 })
