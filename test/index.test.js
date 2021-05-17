e1d1537528552831691a10ec3917bf220e14bea6
Thanh Nguyen <tienthanh8490@gmail.com>
2021-05-17 11:45:20 +0200

    test merge with remove set

--- test/index.test.js
+++ test/index.test.js
@@ -80,8 +80,8 @@ describe('LWWSet', () => {
     })
   })
 
-  describe('basic add and remove', () => {
-    test('should be able to merge two sets', () => {
+  describe('merging', () => {
+    test('should be able to correctly merge two add-only sets', () => {
       lwwSet.add('a', 1)
       lwwSet.add('b', 2)
       lwwSet.add('c', 3)
@@ -103,6 +103,34 @@ describe('LWWSet', () => {
       expect(mergedSet.addSet.get('a')).toBe(5)
       expect(mergedSet.addSet.get('b')).toBe(2)
       expect(mergedSet.addSet.get('c')).toBe(3)
+      expect(mergedSet.addSet.get('d')).toBe(4)
+    })
+
+    test('should be able to correctly merge two sets with both add and remove operations', () => {
+      lwwSet.add('a', 1)
+      lwwSet.add('b', 2)
+      lwwSet.add('c', 3)
+      lwwSet.add('d', 4)
+      lwwSet.remove('b', 5)
+
+      otherSet = new LWWSet()
+
+      otherSet.remove('a', 5)
+      otherSet.add('b', 1)
+      otherSet.add('c', 4)
+      otherSet.remove('d', 3)
+      otherSet.remove('b',6)
+
+      const mergedSet = lwwSet.merge(otherSet)
+
+      expect(mergedSet.lookup('a')).toBe(false)
+      expect(mergedSet.lookup('b')).toBe(false)
+      expect(mergedSet.lookup('c')).toBe(true)
+      expect(mergedSet.lookup('d')).toBe(true)
+
+      expect(mergedSet.removeSet.get('a')).toBe(5)
+      expect(mergedSet.removeSet.get('b')).toBe(6)
+      expect(mergedSet.addSet.get('c')).toBe(4)
       expect(mergedSet.addSet.get('d')).toBe(4)
     })
   })
